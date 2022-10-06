77eb6e2b64150e541949fec90968c56018f795bb
alfonsomiralles <alfonsomiralles1@gmail.com>
2022-10-06 16:51:56 +0200

    creating queries to show data from database

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -1,3 +1,23 @@
+import email
 from django.shortcuts import render
-
+from .models import Author, Entry
 # Create your views here.
+
+def queries(request):
+    # get all the elements
+    authors = Author.objects.all()
+
+    # get filtered elements by condition
+    filtered = Author.objects.filter(email='ryan24@example.org')
+
+    # get an unique object
+    author = Author.objects.get(id=1)
+
+    # get the first 10 objects
+    limits = Author.objects.all()[:10]
+
+    # get the first 5 elements avoiding the first 5 elements
+    offsets = Author.objects.all()[5:10]
+
+    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets})
+
