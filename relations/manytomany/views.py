2c39dac11c3ef80af71b8a268b2894571ac242a8
Fonss <alfonsomiralles1@gmail.com>
2022-11-09 21:20:31 +0100

    relations many to many

---
+++ relations/manytomany/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article,Publication
+# Create your views here.
+
+def create(request):
+    art1 = Article(headline="Noticias de las 1")
+    art1.save()
+    art2 = Article(headline="Noticias de las 2")
+    art2.save()
+    art3 = Article(headline="Noticias de las 3")
+    art3.save()
+
+    pub1 = Publication(title="Publicación 1")
+    pub1.save()
+    pub2 = Publication(title="Publicación 2")
+    pub2.save()
+    pub3 = Publication(title="Publicación 3")
+    pub3.save()
+    pub4 = Publication(title="Publicación 3")
+    pub4.save()
+    pub5 = Publication(title="Publicación 3")
+    pub5.save()
+    pub6 = Publication(title="Publicación 3")
+    pub6.save()
+    pub7 = Publication(title="Publicación 3")
+    pub7.save()
+
+    art1.publications.add(pub1)
+    art1.publications.add(pub2)
+    art1.publications.add(pub3)
+    art2.publications.add(pub4)
+    art2.publications.add(pub5)
+    art3.publications.add(pub6)
+    art3.publications.add(pub7)
+
+    pub1 = Publication.objects.get(id=1)
+    # art1.publications.remove(pub1)
+    # result = art1.publications.all()
+    result = pub1.article_set.all()
+
+
+    return HttpResponse(result)
