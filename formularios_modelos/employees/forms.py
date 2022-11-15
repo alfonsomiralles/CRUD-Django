e8e8a3ea9a0530d7a412918ae7b167a91c048c28
Fonss <alfonsomiralles1@gmail.com>
2022-11-15 19:41:15 +0100

    created forms from models with modelForm

---
+++ formularios_modelos/employees/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Employee
+
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee 
+        fields = ['name','last_name','email']
+        # fields = '__all__'
+        # extra_fields = ['loquesea']
+        # exclude = ('email',)
+
+
+
