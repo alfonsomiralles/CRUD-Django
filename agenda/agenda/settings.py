eec765ab38a84b56d3fcbcadbf44d74357f6b2ba
Alfonso Miralles <62959463+alfonsomiralles@users.noreply.github.com>
2022-11-20 13:10:16 +0100

    Update settings.py

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'agenda',
         'USER': 'postgres',
-        'PASSWORD': 'Doeipoes13',
+        'PASSWORD': 'Tu contraseña',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
