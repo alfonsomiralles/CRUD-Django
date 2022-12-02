58696bcb8142e43f2d8bd66ceca7fbb16b172fbb
Fonss <alfonsomiralles1@gmail.com>
2022-12-02 19:57:53 +0100

    project finished

--- agenda/todo/forms.py
+++ agenda/todo/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from .models import Todo
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
-        fields = '__all__'
+        exclude = ('date',)
+        widgets = {
+            'estimated_end': DateInput(attrs={'type':'date'}),
+        }
